eb01548c2c9a183bb2e24d8532589d67f671016e
Rajkumar Singhal <rajkumar.singhal@bd.com>
2017-02-14 19:06:11 +0530

    Added nav bar design

--- src/app/components/nav/nav-bar.component.ts
+++ src/app/components/nav/nav-bar.component.ts
@@ -2,14 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'nav-bar',
-    template: `
-        <div>
-            <ul>
-                <li>Home</li>
-                <li>Contact</li>
-            </ul>
-        </div>
-    `,
+    templateUrl: 'nav-bar.html',
     styleUrls: ['nav-bar.scss']
 })
 export class NavBarComponent {
